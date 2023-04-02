050b7a296f72d59d9ef7480a59aed60a7d92cfdb
Stan Kad <stanislav.kad@gmail.com>
2023-04-02 19:08:16 +0200

    record route and controller were created, registered in app

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 //here we will describe all app
 const express = require('express');
 const authRoutes = require('./routes/auth');
+const recordRoutes = require('./routes/record')
 const app = express();
 
-app.use('/api/auth', authRoutes)
+app.use('/api/auth', authRoutes);
+app.use('/api/record', recordRoutes);
 
 module.exports = app;
---
+++ server/controllers/record.js
@@ -0,0 +1,30 @@
+module.exports.getAll = (req, res) => {
+    res.status(200).json({
+        signin: true
+    });
+};
+
+module.exports.newRecord = (req, res) => {
+    res.status(200).json({
+        signup: true
+    });
+};
+
+module.exports.getById = (req, res) => {
+    res.status(200).json({
+        signup: true
+    });
+};
+
+module.exports.updRecord = (req, res) => {
+    res.status(200).json({
+        signup: true
+    });
+};
+
+module.exports.deleteRecord = (req, res) => {
+    res.status(200).json({
+        signup: true
+    });
+};
+
