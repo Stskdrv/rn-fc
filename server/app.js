e5234fc6d12d38c113f9259415f72675e200fea4
Stan Kad <stanislav.kad@gmail.com>
2023-04-02 19:26:48 +0200

    overview postman, describe how to use and install parser package

--- server/app.js
+++ server/app.js
@@ -1,8 +1,13 @@
 //here we will describe all app
 const express = require('express');
 const authRoutes = require('./routes/auth');
+const bodyParser = require('body-parser');
 const recordRoutes = require('./routes/record')
 const app = express();
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/record', recordRoutes);
