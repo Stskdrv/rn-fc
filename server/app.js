77b21917a24ccdaf2525e45839d510c23a022f37
Stan Kad <stanislav.kad@gmail.com>
2023-04-03 12:39:59 +0200

    2 packages - morgan and cors were added

--- server/app.js
+++ server/app.js
@@ -2,10 +2,12 @@
 const express = require('express');
 const authRoutes = require('./routes/auth');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const recordRoutes = require('./routes/record')
 const app = express();
 
-
+app.use(morgan('dev'));
+app.use(require('cors')());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
