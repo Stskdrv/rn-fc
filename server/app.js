8fe359c63ae417f5898ce7e60fe266f88fcd43c2
Stan Kad <stanislav.kad@gmail.com>
2023-04-03 17:59:56 +0200

    mongo DB was created and connected to our server

--- server/app.js
+++ server/app.js
@@ -1,10 +1,17 @@
 //here we will describe all app
 const express = require('express');
 const authRoutes = require('./routes/auth');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const recordRoutes = require('./routes/record')
+const recordRoutes = require('./routes/record');
+const { MONGO_URI } = require('../config/keys');
 const app = express();
+
+
+mongoose.connect(MONGO_URI)
+    .then(() => console.log('DB connected'))
+    .catch(e => console.log(e));
 
 app.use(morgan('dev'));
 app.use(require('cors')());
