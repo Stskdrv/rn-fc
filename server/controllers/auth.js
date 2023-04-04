0ac8e8cff6d7f56bbabae786120d07b5384557ec
Stan Kad <stanislav.kad@gmail.com>
2023-04-04 16:41:07 +0200

    done with singup controller logic

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs');
+
 const User = require('../models/User');
 
 
@@ -16,12 +18,32 @@ module.exports.signup = async (req, res) => {
             return res.status(409).json({
                 message: 'Email already exist, please signIn'
             })
+            //If you don't use return statements in these blocks, the function will continue executing and may send multiple responses back to the client, which can result in unexpected behavior or errors. Using return statements ensures that the function exits immediately after sending a response to the client, which is the expected behavior in this case.
         } else {
             return res.status(409).json({
                 message: 'This name already exist, please choose another one.'
             })
         }
     } else {
+        const salt = bcrypt.genSaltSync(10);
+
+        const password = bcrypt.hashSync(req.body.password, salt);
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password,
+        });
+
+        try {
+            await user.save();
+            res.status(201).json({
+                user,
+                message: 'User was created'
+            })
+        }catch(e) {
+            //handle this errer
+        }
+
         
     }
 
