50e7cdeb757c853528bef3c9d367da119571779f
Stan Kad <stanislav.kad@gmail.com>
2023-05-19 13:48:05 +0200

    30-less- ui improvements and service upd

--- client/services/apiClient.js
+++ client/services/apiClient.js
@@ -1,53 +1,54 @@
 import axios from 'axios';
 import * as SecureStore from 'expo-secure-store';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const TOKEN_KEY = 'TOKEN_KEY';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:7000/api',  
-  headers: {
-    'Cache-Control': 'no-cache'
-  },
+    baseURL: 'http://localhost:7000/api',
+    headers: {
+        'Cache-Control': 'no-cache'
+    },
 });
 
 export const setToken = async (token) => {
     await SecureStore.setItemAsync(TOKEN_KEY, token);
 };
 
-const getToken = async (token) => {
-    await SecureStore.getItemAsync(TOKEN_KEY);
+const getToken = async (tokenKey) => {
+    await SecureStore.getItemAsync(tokenKey);
 };
 
 apiClient.interceptors.request.use(
     async (config) => {
         try {
-            const token = getToken(TOKEN_KEY);
-            if (token && config.url !== '/signin/' && config.url !== '/signup/') {
+            const token = await getToken(TOKEN_KEY);
+            if (token && config.url !== 'auth/signin/' && config.url !== 'auth/signup/') {
                 // TODO: check headers
-              config.headers['Authorization'] = `Bearer ${token}`;
+                config.headers['x-authorization'] = `Bearer ${token}`;
             }
             return config;
         } catch (e) {
             console.log(e, 'encrypted');
         }
-      },
-      (error) => {
+    },
+    (error) => {
         return Promise.reject(error);
-      }
-  );
+    }
+);
 
 apiClient.interceptors.response.use(
-  response => {
-    return response;
-  },
-  async error => {
-    if (error.response && error.response.status === 401) {
-      // clear token and set isAuth to false
-      await SecureStore.deleteItemAsync(TOKEN_KEY);
-      await AsyncStorage.setItem('isauth', false);
+    response => {
+        return response;
+    },
+    async error => {
+        if (error.response && error.response.status === 401) {
+            // clear token and set isAuth to false
+            await SecureStore.deleteItemAsync(TOKEN_KEY);
+            await AsyncStorage.setItem('isauth', false);
+        }
+        return Promise.reject(error);
     }
-    return Promise.reject(error);
-  }
 );
 
 export default apiClient;
--- client/services/authService.js
+++ client/services/authService.js
@@ -1,32 +1,22 @@
-import apiClient, { setToken } from "./apiClient";
+import apiClient from "./apiClient";
 
-
-export const signUp = async ({ email, password, name }) => {
+export const signUp = async ({ name, email, password }) => {
     const params = {
-        email,
-        password,
         name,
-      };
+        email,
+        password
+    };
 
-    return await apiClient.post('auth/signup/', params)
-        .then((res) => console.log(res.data))
-        .catch(e => console.log(e))
-        .finally(() => console.log('finish'));
+    return await apiClient.post('auth/signup/', params);
 };
 
-export const signIn = async ({ email, password, name }) => {
-    const params = {
-        email,
-        password,
-        name,
-      };
 
-    return await apiClient.post('auth/signin/', params)
-        .then((res) => {
-            const token = res.data.token;
-            console.log(token);
-            setToken(token);
-        })
-        .catch(e => console.log(e))
-        .finally(() => console.log('finish'));
+export const signIn = async ({ name, email, password }) => {
+    const params = {
+        name,
+        email,
+        password
+    };
+
+    return await apiClient.post('auth/signin/', params);
 };
