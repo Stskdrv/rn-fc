7587dc2a46ac2691eaa2156fcb0f4f11c5e50322
Stan Kad <stanislav.kad@gmail.com>
2023-05-25 22:40:26 +0200

    Whole prepearing for working with api was done, set name dynamically, installes nessesary packages

--- client/screens/SignInScreen.jsx
+++ client/screens/SignInScreen.jsx
@@ -1,9 +1,9 @@
 import { Image, StyleSheet, Text, View } from 'react-native';
 import * as SecureStore from 'expo-secure-store';
 import AuthForm from '../components/AuthForm';
-import { Button } from 'native-base';
+import { Button, Toast } from 'native-base';
 import { signIn } from '../services/authService';
-import { setToken } from '../services/apiClient';
+import { setToken, setUserName } from '../services/apiClient';
 
 export default SignInScreen = ({ navigation }) => {
 
@@ -17,9 +17,9 @@ export default SignInScreen = ({ navigation }) => {
     }
     await signIn(values)
       .then(async (response) => {
-        const token = response.data.token;
-        console.log(token);
+        const { token, name } = response.data;
         await setToken(token);
+        await setUserName(name);
       })
       .catch((error) => {
         Toast.show({
@@ -35,7 +35,7 @@ export default SignInScreen = ({ navigation }) => {
       <Image style={styles.logo} source={require('../assets/logoIcon.png')} />
       <AuthForm handleSubmit={handleSubmit} type='SignIn' />
       <Text style={styles.text}>In you do nota have an account, please</Text>
-      <Button mt='5' alignSelf='center' w='35%' onPress={() => navigation.navigate('SignUp')}>go to Sign Up</Button>
+      <Button mt='5' alignSelf='center' w='35%' rounded='full' onPress={() => navigation.navigate('SignUp')}>go to Sign Up</Button>
     </View>
   );
 }
